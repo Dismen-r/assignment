ef14d7e8047e1f542daf245d2362ab63a5e9c891
Dismen-r <dismemr25@gmail.com>
2022-03-08 19:32:06 +0530

    assignment

---
+++ Assignment1/src/main/java/Assignment1/App.java
@@ -0,0 +1,28 @@
+package Assignment1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("OnlineShop.xml");
+		User customer = context.getBean("u1", User.class);
+		System.out.println(customer.getUsername());
+		System.out.println(customer.getEmail());
+		System.out.println(customer.getShipping_address());
+		
+		
+		Product p = context.getBean("p1", Product.class);
+		System.out.println(p.getProduct_name());
+		System.out.println(p.getPrice());
+		
+		Cart c=context.getBean("c1", Cart.class);
+		System.out.println(c.getTotal_amount());
+		System.out.println(c.getStatus());
+		
+		Order o=context.getBean("o1", Order.class);
+		System.out.println(o.getCartList());
+		
+	
+	}
+}
---
+++ Assignment1/src/main/java/Assignment1/User.java
@@ -0,0 +1,59 @@
+package Assignment1;
+
+public class User {
+
+	private String username, email, password, shipping_address, billing_address;
+
+	public User() {
+		super();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getShipping_address() {
+		return shipping_address;
+	}
+
+	public void setShipping_address(String shipping_address) {
+		this.shipping_address = shipping_address;
+	}
+
+	public String getBilling_address() {
+		return billing_address;
+	}
+
+	public void setBilling_address(String billing_address) {
+		this.billing_address = billing_address;
+	}
+
+	public User(String username, String email, String password, String shipping_address, String billing_address) {
+		super();
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.shipping_address = shipping_address;
+		this.billing_address = billing_address;
+	}
+}
